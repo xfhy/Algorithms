d4b60c00f7908c100481d371b137afa572118b2b
xfhy <xfhy666@qq.com>
2020-07-05 16:30:03 +0800

    二位数组中的查找

--- DataStructures/src/array/Solution.java
+++ DataStructures/src/array/Solution.java
@@ -1,46 +1,53 @@
 package array;
 
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * @author : xfhy
- * Create time : 2020年07月04日14:28:52
- * Description : 剑指 Offer 03. 数组中重复的数字
+ * Create time : 2020年07月05日10:20:18
+ * Description : 剑指 Offer 04. 二维数组中的查找
  */
 public class Solution {
     /**
-     * 解法1:将nums数组的值挨个存入集合,存入时判断集合中是否存在该元素,存在则找到了.
+     * 暴力 2层循环,找二维数组中的每一个元素
      */
-    public static int findRepeatNumber(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        for (int num : nums) {
-            if (!set.add(num)) {
-                return num;
+    public static boolean findNumberIn2DArray(int[][] matrix, int target) {
+        for (int[] ints : matrix) {
+            for (int anInt : ints) {
+                if (target == anInt) {
+                    return true;
+                }
             }
         }
-        return 0;
+        return false;
     }
 
     /**
-     * 解法2: 定义1个长度为100000的数组,因为nums的所有元素都在2-100000的范围内.
-     * 将nums[i]的值与数组下标对应,循环nums数组,将数组相应位置+1.循环时遇到数组不为0的则找到了
+     *
      */
-    public static int findRepeatNumber2(int[] nums) {
-        int[] array = new int[100000];
-        for (int num : nums) {
-            if (array[num] != 0) {
-                return num;
+    public static boolean findNumberIn2DArray2(int[][] matrix, int target) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (target < matrix[i][j]) {
+                    break;
+                }
+                if (target == matrix[i][j]) {
+                    return true;
+                }
             }
-            array[num]++;
         }
-        return 0;
+        return false;
     }
 
     public static void main(String[] args) {
        /* int heightChecker = heightChecker(new int[]{1, 2, 3, 4, 5});
         System.out.println(heightChecker);*/
-        System.out.println(findRepeatNumber(new int[]{2, 3, 1, 0, 2, 5, 3}));
+        int[][] array = new int[][]{
+                {1, 4, 7, 11, 15},
+                {2, 5, 8, 12, 19},
+                {3, 6, 9, 16, 22},
+                {10, 13, 14, 17, 24},
+                {18, 21, 23, 26, 30}
+        };
+        System.out.println(findNumberIn2DArray2(array, 5));
 
     }
 
