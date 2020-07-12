bccd8ef4953bce9871099d8d918e1a3203c806bd
xfhy <xfhy666@qq.com>
2020-07-12 10:20:09 +0800

    二叉树的创建和遍历

---
+++ DataStructures/src/tree/BinaryTree.java
@@ -0,0 +1,66 @@
+package tree;
+
+import java.util.LinkedList;
+
+/**
+ * @author : xfhy
+ * Create time : 2020/7/12 9:51 上午
+ * Description : 二叉树操作
+ */
+public class BinaryTree {
+
+    /**
+     * 二叉树节点
+     */
+    public static class TreeNode {
+        public int data;
+        public TreeNode leftChild;
+        public TreeNode rightChild;
+
+        public TreeNode(int data) {
+            this.data = data;
+        }
+    }
+
+    public TreeNode createBinaryTree(LinkedList<Integer> inputList) {
+        if (inputList == null || inputList.isEmpty()) {
+            return null;
+        }
+        TreeNode node = null;
+        Integer data = inputList.removeFirst();
+        if (data != null) {
+            node = new TreeNode(data);
+            node.leftChild = createBinaryTree(inputList);
+            node.rightChild = createBinaryTree(inputList);
+        }
+        return node;
+    }
+
+    public void preOrderTraveral(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        System.out.println(node.data);
+        preOrderTraveral(node.leftChild);
+        preOrderTraveral(node.rightChild);
+    }
+
+    public void inOrderTraveral(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        inOrderTraveral(node.leftChild);
+        System.out.println(node.data);
+        inOrderTraveral(node.rightChild);
+    }
+
+    public void postOrderTraveral(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        postOrderTraveral(node.leftChild);
+        postOrderTraveral(node.rightChild);
+        System.out.println(node.data);
+    }
+
+}
---
+++ DataStructures/src/tree/BinaryTreeTest.java
@@ -0,0 +1,27 @@
+package tree;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+/**
+ * @author : xfhy
+ * Create time : 2020/7/12 10:01 上午
+ * Description :
+ */
+public class BinaryTreeTest {
+
+    public static void main(String[] args) {
+        BinaryTree binaryTree = new BinaryTree();
+        LinkedList<Integer> integers = new LinkedList<>(Arrays.asList(3, 2, 9, null, null, 10, null,
+                null, 8, null, 4));
+
+        BinaryTree.TreeNode treeNode = binaryTree.createBinaryTree(integers);
+        System.out.println("前序遍历");
+        binaryTree.preOrderTraveral(treeNode);
+        System.out.println("中序遍历");
+        binaryTree.inOrderTraveral(treeNode);
+        System.out.println("后序遍历");
+        binaryTree.postOrderTraveral(treeNode);
+    }
+
+}
