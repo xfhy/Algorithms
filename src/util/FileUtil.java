8c1a3c9ca460ee569bf8096201bdd120ee5d1d24
xfhy <xfhy666@qq.com>
2020-08-15 13:49:33 +0800

    update README.md

--- src/util/FileUtil.java
+++ src/util/FileUtil.java
@@ -1,6 +1,8 @@
 package util;
 
 import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -31,7 +33,7 @@ public class FileUtil {
                         .replaceAll(" ", "_");*/
                 StringBuilder stringBuilder = new StringBuilder();
                 stringBuilder.append("- ").append("[")
-                        .append(file1.getName().replaceAll("\\.md","").replaceAll("_"," "))
+                        .append(file1.getName().replaceAll("\\.md", "").replaceAll("_", " "))
                         .append("]")
                         .append("(")
                         .append(File.separator)
@@ -41,7 +43,26 @@ public class FileUtil {
                         .append(File.separator)
                         .append(file1.getName())
                         .append(")");
+
+                StringBuilder url = new StringBuilder();
+                url.append(File.separator)
+                        .append("doc")
+                        .append(File.separator)
+                        .append(dir.getName())
+                        .append(File.separator)
+                        .append(file1.getName());
+
+                String result = "";
+                try {
+                    result = URLEncoder.encode(url.toString(), "UTF-8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                result = result.replaceAll("%3A", ":").replaceAll("%2F", "/").replaceAll("\\+", "%20");//+实际上是 空格 url encode而来
+
                 System.out.println(stringBuilder.toString());
+                System.out.println(result);
+
 
                 //boolean renameTo = file1.renameTo(new File(dir.getAbsolutePath(), file1Name));
             }
