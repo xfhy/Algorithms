68fbe7c57ab027e147e6b8cff0445772a1b5a5c8
xfhy <xfhy666@qq.com>
2021-05-19 09:29:36 +0800

    打开转盘锁 未完成

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,4 @@
-import java.util.Arrays;
-import java.util.LinkedList;
+import java.util.*;
 
 
 /**
@@ -9,108 +8,74 @@ import java.util.LinkedList;
  * source : https://leetcode-cn.com/problems/minimum-depth-of-binary-tree/
  */
 public class Solution {
-    public static class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
 
-        TreeNode(int x) {
-            val = x;
-        }
+    //打开转盘锁
+    //从"0000"开始,转第一次,有8种可能,分别是"1000","9000","0100","0900"...
+    //可以把"0000"看成是根节点,然后可以把题目看成图,从根节点出发,每个节点都有8个相邻的节点.
+    //首先穷举完所有可能的结果,然后根据题目意思 跳过 已经访问过的+不允许访问的
+
+    int openLock(String[] deadends, String target) {
+        return -1;
     }
 
-    //BFS 框架
-    /*
-
-    //计算从起点start到终点target的最短距离
-        int BFS(Node start, Node target) {
-            //核心数据结构
-            Queue<Node> q;
-            //避免走回头路
-            Set<Node> visited;
-
-            //将起点加入
-            q.offer(start);
-            visited.add(start);
-            //记录扩散的步数
-            int step = 0;
-
-            while (q not empty){
-                int sz = q.size();
-                //将当前队列中的所有节点向四周扩散
-                for (int i = 0; i < sz; i++) {
-                    Node cur = q.poll();
-                    //判断是否已经到达终点
-                    if (cur is target)return step;
-                    //将cur相邻节点加入队列
-                    for (Node x : cur.adj()) {
-                        if(x not in visited) {
-                            q.offer(x);
-                            visited.add(x);
-                        }
-                    }
-                }
-                //!!!更新步数
-                step++;
-            }
+    /**
+     * 将s[j]向上拨动一次
+     */
+    String plusOne(String s, int j) {
+        char[] chars = s.toCharArray();
+        if (chars[j] == '9') {
+            chars[j] = '0';
+        } else {
+            chars[j]++;
         }
+        return new String(chars);
+    }
 
-    * */
+    /**
+     * 将s[j]向下拨动一次
+     */
+    String minusOne(String s, int j) {
+        char[] chars = s.toCharArray();
+        if (chars[j] == '0') {
+            chars[j] = '9';
+        } else {
+            chars[j]--;
+        }
+        return new String(chars);
+    }
 
-    //二叉树的最小深度
-    //使用BFS 广度优先搜索 ,空间复杂度稍微高一些
-    //使用DFS 深度优先搜索, 空间复杂度稍微低一些
+    //这是代码框架
+    //缺陷1: 会走回头路. 比如0000拨到1000,但是等从队列中拿出1000时,还会拨出一个0000,如此往复,产生死循环
+    //缺陷2: 没有终止条件,按照题目要求,找到target就应该结束并返回拨动的次数
+    //缺陷3: 没有对deadends的处理,按道理这些死亡密码是不能出现的,也就是说遇到这些密码需要跳过,不进行操作
+    void BFS(String target) {
+        Queue<String> queue = new LinkedList<>();
+        queue.add("0000");
 
-    //这里使用BFS框架
-    int minDepth(TreeNode root) {
-        if (root == null) return 0;
-        LinkedList<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        //root本身占一层,所以初始化为1
-        int depth = 1;
         while (!queue.isEmpty()) {
             int size = queue.size();
-            //将当前队列中的所有节点向四周扩散
+            //将当前队列的所有节点向周围扩散
             for (int i = 0; i < size; i++) {
-                TreeNode cur = queue.poll();
+                String poll = queue.poll();
                 //判断是否到达终点
-                if (cur.left == null && cur.right == null) {
-                    return depth;
-                }
-                //将cur相邻节点加入队列
-                if (cur.left != null) {
-                    queue.offer(cur.left);
-                }
-                if (cur.right != null) {
-                    queue.offer(cur.right);
+                System.out.println(poll);
+
+                //将一个节点的相邻节点加入队列
+                for (int j = 0; j < 4; j++) {
+                    //将s[j]向上拨动一次  一个新密码形成
+                    String plusOneNewPwd = plusOne(poll, j);
+                    String minusOneNewPwd = minusOne(poll, j);
+                    queue.offer(plusOneNewPwd);
+                    queue.offer(minusOneNewPwd);
                 }
             }
-            //将这一层的全部添加完成  增加步数
-            depth++;
+            //在这里增加步数
         }
-        return depth;
-    }
-
-    public static TreeNode createBinaryTree(LinkedList<Integer> inputList) {
-        if (inputList == null || inputList.isEmpty()) {
-            return null;
-        }
-        TreeNode node = null;
-        Integer data = inputList.removeFirst();
-        if (data != null) {
-            node = new TreeNode(data);
-            node.left = createBinaryTree(inputList);
-            node.right = createBinaryTree(inputList);
-        }
-        return node;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        LinkedList<Integer> integers = new LinkedList<>(Arrays.asList(3, 9, null, null, 20, 15, null, null, 7));
-        TreeNode binaryTree = createBinaryTree(integers);
-
-        System.out.println(solution.minDepth(binaryTree));
+        solution.BFS("0100");
     }
 
 }
