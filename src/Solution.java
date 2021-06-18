dc2218cf5c61b607db59a1e7da89ec446968dc9f
xfhy <xfhy666@qq.com>
2021-06-18 09:09:37 +0800

    滑动窗口算法框架+最小覆盖子串

--- src/Solution.java
+++ src/Solution.java
@@ -1,73 +1,79 @@
+import java.util.HashMap;
 
 /**
  * @author : xfhy
- * Create time : 2021年06月08日08:58:46
- * Description : 二分查找
+ * Create time : 2021年06月17日08:58:46
+ * Description : 滑动窗口
  * source :
  */
 public class Solution {
 
     /**
-     * 二分查找基本框架
+     * 最小覆盖子串
+     *
+     * @param s 全部字符串
+     * @param t 需要在s中找到包含t的最小子串
      */
-    int binarySearch(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] > target) {
-                right = mid - 1;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            }
+    public String minWindow(String s, String t) {
+        HashMap<String, Integer> need = new HashMap<>(), window = new HashMap<>();
+        for (int i = 0; i < t.length(); i++) {
+            String c = String.valueOf(t.charAt(i));
+            need.put(c, need.getOrDefault(c, 0) + 1);
         }
-        return -1;
-    }
 
-    int left_bound(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;   //left+right太大了，防止溢出
-            if (nums[mid] == target) {
-                right = mid - 1;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else if (nums[mid] > target) {
-                right = mid - 1;
+        int left = 0, right = 0;
+        //窗口中满足need条件的字符个数
+        int valid = 0;
+        //记录最小字串的起始索引及长度
+        int start = 0, len = Integer.MAX_VALUE;
+        while (right < s.length()) {
+            //c是将移入窗口的字符
+            String c = String.valueOf(s.charAt(right));
+            //右移窗口
+            right++;
+            //进行窗口内数据的一系列更新   need中有这个字符
+            if (need.get(c) != null) {
+                //窗口中相应字符个数+1
+                window.put(c, window.getOrDefault(c, 0) + 1);
+                //如果窗口中该字符个数与need中该字符个数相等,则说明该字符收集满了
+                if (need.get(c).equals(window.get(c))) {
+                    valid++;
+                }
             }
-        }
-        //出循环时 left = right+1
-        if (left >= nums.length || nums[left] != target) {
-            return -1;
-        }
-        return left;
-    }
 
-    int right_bound(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (target == nums[mid]) {
-                //收缩左侧边界
-                left = mid + 1;
-            } else if (target > nums[mid]) {
-                left = mid + 1;
-            } else if (target < nums[mid]) {
-                right = mid - 1;
+            System.out.printf("window: [%d,%d)", left, right);
+
+            //need中的字符全部都收集齐了,则收缩左侧窗口
+            while (valid == need.size()) {
+                //在这里更新最小覆盖子串
+                if (right - left < len) {
+                    start = left;
+                    len = right - left;
+                }
+                //d是将移出窗口的字符
+                String d = String.valueOf(s.charAt(left));
+                //左移窗口
+                left++;
+                //进行窗口内数据的一系列更新   -> need含有该字符,才需要更新窗口内数据
+                if (need.get(d) != null) {
+                    //need中的该字符个数比窗口中该字符个数相等,则需要将valid-1
+                    if (need.get(d).equals(window.get(d))) {
+                        valid--;
+                    }
+                    //窗口中相应字符个数-1
+                    window.put(d, window.get(d) - 1);
+                }
             }
         }
-        //出循环时  right=left-1
-        if (right < 0 || nums[right] != target) return -1;
-        return right;
+        return len == Integer.MAX_VALUE ? "" : s.substring(start, start + len);
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        //System.out.println(solution.left_bound(new int[]{1, 3, 5/*, 5, 5, 5*/, 6}, 5));
-        System.out.println(solution.right_bound(new int[]{1, 3, 5/*, 5, 5, 5*/, 6}, 1));
-
+        String minWindow = solution.minWindow("AABBCCDGEDDS", "BCA");
+        System.out.println("\n");
+        System.out.println(minWindow);
     }
 
 }
