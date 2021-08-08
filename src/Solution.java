ff16b3ce9cdf6372b63160b8b0513444ef1ef417
xfhy <xfhy666@qq.com>
2021-08-09 06:59:56 +0800

    最长公共子序列的长度  dp table 备忘录解法

--- src/Solution.java
+++ src/Solution.java
@@ -32,10 +32,28 @@ public class Solution {
         }
     }
 
+    //2. dp table 备忘录解法
+    int longestCommonSubsequence2(String str1, String str2) {
+        int m = str1.length();
+        int n = str2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.longestCommonSubsequence("abcde", "aceb"));
+//        System.out.println(solution.longestCommonSubsequence("abcde", "aceb"));
+        System.out.println(solution.longestCommonSubsequence2("abcde", "eceb"));
     }
 
 }
